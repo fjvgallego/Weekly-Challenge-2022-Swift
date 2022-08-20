c1abc64d4e2198683b75b04fc889ea01873569a7
Francisco Javier Gallego Lahera <fgallego19@hotmail.com>
2022-08-20 22:18:48 +0200

    Solución Reto #33

--- WeeklyChallenge2022.playground/Pages/Challenge33.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge33.xcplaygroundpage/Contents.swift
@@ -22,3 +22,89 @@ import Foundation
  *
  */
 
+/// CALLS
+
+(1984...2043).forEach(getChineseEquivalent(for:))
+
+/// SETUP
+
+struct Constants {
+    static let kStartingCycleReferenceYear = 1924
+}
+
+protocol ChineseItem: CaseIterable, CustomStringConvertible, Hashable {
+    static var numberOfYearsInARow: Int { get set }
+}
+
+enum WuXing: ChineseItem {
+    case wood, fire, grass, metal, water
+    
+    var description: String {
+        switch self {
+        case .wood: return "Wood"
+        case .fire: return "Fire"
+        case .grass: return "Earth"
+        case .metal: return "Metal"
+        case .water: return "Water"
+        }
+    }
+    
+    static var numberOfYearsInARow = 2
+}
+
+enum ZodiacSign: ChineseItem {
+    case rat, ox, tiger, rabbit, dragon, snake, horse, sheep, monkey, rooster, dog, pig
+    
+    var description: String {
+        switch self {
+        case .rat: return "Rat"
+        case .ox: return "Ox"
+        case .tiger: return "Tiger"
+        case .rabbit: return "Rabbit"
+        case .dragon: return "Dragon"
+        case .snake: return "Snake"
+        case .horse: return "Horse"
+        case .sheep: return "Sheep"
+        case .monkey: return "Monkey"
+        case .rooster: return "Rooster"
+        case .dog: return "Dog"
+        case .pig: return "Pig"
+        }
+    }
+    
+    static var numberOfYearsInARow = 1
+}
+
+/// Given a positive year, this method returns the corresponding Chinese items: WuXing and ZodiacSign.
+func getChineseEquivalent(for year: Int) {
+    // Validations
+    guard year >= 0 else {
+        print("[ERROR] The year to calculate \(year) must be positive.")
+        return
+    }
+    
+    // Calculations
+    let yearWuXing: WuXing = getItem(for: year)
+    let yearZodiacSign: ZodiacSign = getItem(for: year)
+    
+    // Final Result
+    print("[\(year)] WuXing: \(yearWuXing) - ZodiacSign: \(yearZodiacSign)")
+}
+
+/// Given a positive year, this method returns the corresponding item.
+func getItem<T: ChineseItem>(for year: Int) -> T {
+    let yearDifference = year - Constants.kStartingCycleReferenceYear
+    let numberOfItems = T.allCases.count
+    
+    var itemYearIndex = (Int(yearDifference/T.numberOfYearsInARow)) % numberOfItems
+    
+    // Check the given year is more recent than the starting cycle reference year.
+    // Otherwise, calculate the positive difference, and consequently the positive index for the item.
+    if yearDifference < 0 {
+        itemYearIndex = numberOfItems - itemYearIndex
+    }
+    
+    let yearItem = Array(T.allCases)[itemYearIndex]
+    
+    return yearItem
+}
