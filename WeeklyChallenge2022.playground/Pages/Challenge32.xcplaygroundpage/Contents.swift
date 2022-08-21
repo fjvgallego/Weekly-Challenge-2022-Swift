76a0438687fd129c2733f4d2e1378c8e6a3d9c37
Francisco Javier Gallego Lahera <fgallego19@hotmail.com>
2022-08-21 13:44:27 +0200

    Solución Reto #32

--- WeeklyChallenge2022.playground/Pages/Challenge32.xcplaygroundpage/Contents.swift
+++ WeeklyChallenge2022.playground/Pages/Challenge32.xcplaygroundpage/Contents.swift
@@ -15,3 +15,38 @@ import Foundation
  *
  */
 
+findSecondGreatestNumber(in: [3, 10, 2, 4, 8, 7, 5])
+findSecondGreatestNumber(in: [1, 2, 3, 4, 4, 6, 7, 9, 9, 10])
+findSecondGreatestNumber(in: [1, 1, 1])
+
+/// Finds the second greatest number in an integer array, even if it's repeated among other numbers.
+/// * The number list must be formed by, at least, two numbers.
+/// * No second greatest number is printed if the list only contains the same number repeated more than once (e.g: [1, 1, 1]).
+func findSecondGreatestNumber(in numberList: [Int]) {
+    guard numberList.count >= 2 else {
+        print("[ERROR] The number list must contain, at least, two numbers.")
+        return
+    }
+    
+    var maxNumber = numberList.first!
+    var secondMaxNumber: Int?
+    
+    for loopNumber in numberList[1...] {
+        if loopNumber > maxNumber {
+            secondMaxNumber = maxNumber
+            maxNumber = loopNumber
+        } else {
+            if secondMaxNumber != nil {
+                if loopNumber > secondMaxNumber! {
+                    secondMaxNumber = loopNumber
+                }
+            }
+        }
+    }
+    
+    if let secondMaxNumber = secondMaxNumber {
+        print("The second greater number is: \(secondMaxNumber)")
+    } else {
+        print("No second greater number found. Maybe the number list contains the same number repeated several times.")
+    }
+}
